67df37d6ac3166a5f187c9e36adc97d5f4f1791d
schmidtw <Weston_Schmidt@alumni.purdue.edu>
2023-08-04 23:18:11 -0700

    Add the examples from wrp-listener.

---
+++ timedListener/acquirer.go
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2019 Comcast Cable Communications Management, LLC
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import "github.com/xmidt-org/bascule/acquire"
+
+// determineTokenAcquirer always returns a valid TokenAcquirer
+func determineTokenAcquirer(config WebhookConfig) (acquire.Acquirer, error) {
+	defaultAcquirer := &acquire.DefaultAcquirer{}
+	if config.JWT.AuthURL != "" && config.JWT.Buffer != 0 && config.JWT.Timeout != 0 {
+		return acquire.NewRemoteBearerTokenAcquirer(config.JWT)
+	}
+
+	if config.Basic != "" {
+		return acquire.NewFixedAuthAcquirer("Basic " + config.Basic)
+	}
+
+	return defaultAcquirer, nil
+}
